5bbaf84005f9d7a04538638312aeb79be8f7159c
Oliver Jasiel Galaviz Arroyo <oliver.jga@gmail.com>
2016-02-01 21:03:37 -0700

    Update Hola_pablo.java

--- Hola_pablo.java
+++ Hola_pablo.java
@@ -1,7 +1,13 @@
 public class jelou(){
 	
+	public void cotorreando (){
+		System.out.println("Aqui nada mas estamos cotorreando");
+	}
+	
 	String cad ="El OLiver es Gay";
 	public static void main(String[] args){
+		public jelou ob = new jelou ();
+		a.cotorreando();
 		system.out.println("Oliver es homosexual" + "\n"+cad);
 	}
 }
