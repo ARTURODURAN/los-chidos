f8624b0e9d5228fd205fc295264f952cf0ed85c0
“Napstyle” <nap_laluz@hotmail.com>
2016-02-02 15:18:37 -0700

    Le puse que OLiver es puto

--- Hola_pablo.java
+++ Hola_pablo.java
@@ -4,10 +4,10 @@ public class jelou(){
 		System.out.println("Aqui nada mas estamos cotorreando");
 	}
 	
-	String cad ="El OLiver es mi macho";
+	String cad ="El OLiver es bien puto";
 	public static void main(String[] args){
 		public jelou ob = new jelou ();
 		a.cotorreando();
-		system.out.println("Oliver es mi macho" + "\n"+cad);
+		system.out.println("Oliver es bien puto" + "\n"+cad);
 	}
 }
